47987432a5668a391a88e0435ed551bab25fe6b9
Renato <renato.f.fabri@gmail.com>
2024-05-01 23:31:24 -0300

    Add auth config for convex

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,4 +1,4 @@
-export default {
+const auth_config = {
   providers: [
     {
       domain: "https://definite-ram-29.clerk.accounts.dev",
@@ -6,3 +6,5 @@ export default {
     },
   ],
 };
+
+export default auth_config;
