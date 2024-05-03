106b6071c47df2357ce3ff071dca6c97722f46ae
Renato <renato.f.fabri@gmail.com>
2024-05-03 20:19:35 -0300

    Add navbar and sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from "./_components/navbar";
 import Sidebar from "./_components/sidebar";
+import OrgSidebar from "./_components/sidebar/org-sidebar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -8,7 +10,15 @@ function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
