78d6516043a8282b6c690dab5394e1ed06efeee7
Renato <renato.f.fabri@gmail.com>
2024-05-03 17:49:36 -0300

    ADD Loading.tsx and logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/protected1(.*)",
+  "/protected2(.*)",
+  "/(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect((has) => {
-      return (
-        has({ permission: "org:sys_memberships:manage" }) ||
-        has({ permission: "org:sys_domains_manage" })
-      );
-    });
-  }
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
