8df98e44ae3b5425da38109d4bc697a2bf52e270
Renato <renato.f.fabri@gmail.com>
2024-05-03 17:10:17 -0300

    FIX Middleware.ts not logging in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher(["/(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect((has) => {
+      return (
+        has({ permission: "org:sys_memberships:manage" }) ||
+        has({ permission: "org:sys_domains_manage" })
+      );
+    });
+  }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
