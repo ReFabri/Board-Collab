710acd117a15245358f4f0bf9a892734fc5e75a0
Renato <renato.f.fabri@gmail.com>
2024-05-04 20:50:43 -0300

    Changes to sidebar and middleware

--- src/app/(dashboard)/_components/sidebar/index.tsx
+++ src/app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NewButton from "./new-button";
 
 function Sidebar() {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 text-white">
-      Side
+      <NewButton />
     </aside>
   );
 }
