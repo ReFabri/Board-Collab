0f3a9eba83ef597e091bbd443819c866a0ed1397
Renato <renato.f.fabri@gmail.com>
2024-05-06 19:49:00 -0300

    Add Item component

--- src/app/(dashboard)/_components/sidebar/index.tsx
+++ src/app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import List from "./List";
 import NewButton from "./NewButton";
 
 function Sidebar() {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 text-white">
+      <List />
       <NewButton />
     </aside>
   );
